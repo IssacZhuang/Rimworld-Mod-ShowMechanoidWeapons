a84dc342d01ec481b7f307d4396379324d3ffb49
zhuang jr <1095313400@qq.com>
2024-04-08 20:40:32 +0800

    first commit

---
+++ Source/1.5/ShowMechanoidWeapon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Verse;
+using RimWorld;
+
+namespace ShowMechanoidWeapon
+{
+
+    public class ShowMechanoidWeapon : Mod
+    {
+        public ShowMechanoidWeapon(ModContentPack content) : base(content)
+        {
+            LongEventHandler.QueueLongEvent(new Action(ShowMechanoidWeapon.InjectHyperLink), "SMW_InjectHyperLink", false, null, true);
+        }
+
+        public static void InjectHyperLink()
+        {
+            IEnumerable<PawnKindDef> mechs = DefDatabase<PawnKindDef>.AllDefsListForReading.AsParallel().Where(x => x.race?.race?.FleshType == FleshTypeDefOf.Mechanoid);
+            foreach (var mech in mechs)
+            {
+                ThingDef race = mech.race;
+                if (race.descriptionHyperlinks == null) race.descriptionHyperlinks = new List<DefHyperlink>();
+
+                CompProperties_TurretGun comp = (CompProperties_TurretGun)race.comps?.FirstOrDefault(x => x is CompProperties_TurretGun);
+                if (comp?.turretDef != null) race.descriptionHyperlinks.Add(new DefHyperlink { def = comp.turretDef });
+
+                if (mech.weaponTags.NullOrEmpty()) continue;
+
+                IEnumerable<ThingDef> mechWeapons = DefDatabase<ThingDef>.AllDefsListForReading.AsParallel().Where(x => x.weaponTags != null && x.weaponTags.Contains(mech.weaponTags.First()));
+
+                foreach(ThingDef weapon in mechWeapons)
+                {
+                    race.descriptionHyperlinks.Add(new DefHyperlink
+                    {
+                        def = weapon
+                    });
+                }
+
+               
+            }
+        }
+    }
+}
